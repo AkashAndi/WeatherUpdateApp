87ea43fbc8eadd195d7a3ffe25f2dc4243965347
bluelabeldeveloper1 <rajendra@bluelabellabs.com>
2020-09-25 23:55:17 +0530

    code refactoring

--- app/src/main/java/com/app/weatherupdates/utils/FragmentManagerExtension.kt
+++
@@ -1,8 +0,0 @@
-package com.app.weatherupdates.utils
-
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-
-fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
-    beginTransaction().func().commit()
-}
--- app/src/main/java/com/app/weatherupdates/utils/ViewExtension.kt
+++ app/src/main/java/com/app/weatherupdates/utils/ViewExtension.kt
@@ -1,38 +1,12 @@
 package com.app.weatherupdates.utils
 
-import android.content.Context
 import android.view.View
 import android.widget.TextView
-import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 
 
-fun View.show() {
-    this.visibility = View.VISIBLE
-}
-
 fun View.gone() {
     this.visibility = View.GONE
-}
-
-fun View.invisible() {
-    this.visibility = View.INVISIBLE
-}
-
-fun View.enable() {
-    this.isEnabled = true
-}
-
-fun View.disable() {
-    this.isEnabled = false
-}
-
-fun makeLongToast(context: Context, message: String) {
-    Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-}
-
-fun makeShortToast(context: Context, message: String) {
-    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
 
 fun View.showSnack(message: String, length: Int = Snackbar.LENGTH_LONG) {
