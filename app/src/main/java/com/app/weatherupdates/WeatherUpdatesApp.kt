bf7a2197a4b5ff2c83e8e9705bebf34e1d9f40be
bluelabeldeveloper1 <rajendra@bluelabellabs.com>
2020-09-25 23:58:14 +0530

    removed unused instance

--- app/src/main/java/com/app/weatherupdates/WeatherUpdatesApp.kt
+++ app/src/main/java/com/app/weatherupdates/WeatherUpdatesApp.kt
@@ -6,17 +6,9 @@ import dagger.android.DaggerApplication
 
 class WeatherUpdatesApp : DaggerApplication() {
 
-    companion object {
-        lateinit var instance: WeatherUpdatesApp
-    }
-
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().create(this)
     }
 
 
-    override fun onCreate() {
-        super.onCreate()
-        instance = this
-    }
 }
